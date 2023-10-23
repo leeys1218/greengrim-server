bce0144318ae4f99f25321947205e9976493447e
YeonSang <leeys12181811@gmail.com>
2023-10-23 21:00:23 +0900

    feat/#28/chatting

--- green/src/main/java/com/greengrim/green/core/chatRoom/ChatRoom.java
+++ green/src/main/java/com/greengrim/green/core/chatRoom/ChatRoom.java
@@ -1,35 +1,20 @@
 package com.greengrim.green.core.chatRoom;
 
-import com.greengrim.green.core.chat.ChatService;
-import com.greengrim.green.core.chat.dto.ChatMessage;
-import com.greengrim.green.core.chat.dto.ChatMessage.MessageType;
-import java.util.HashSet;
-import java.util.Set;
-import lombok.Builder;
+import java.util.UUID;
 import lombok.Getter;
-import org.springframework.web.socket.WebSocketSession;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ChatRoom {
   private String roomId;
   private String name;
-  private Set<WebSocketSession> sessions = new HashSet<>();
 
-  @Builder
-  public ChatRoom(String roomId, String name) {
-    this.roomId = roomId;
-    this.name = name;
+  public static ChatRoom create(String name) {
+    ChatRoom chatRoom = new ChatRoom();
+    chatRoom.roomId = UUID.randomUUID().toString();
+    chatRoom.name = name;
+    return chatRoom;
   }
 
-  public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-    if(chatMessage.getType().equals(MessageType.ENTER)) {
-      sessions.add(session);
-      chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
-    }
-    sendMessage(chatMessage, chatService);
-  }
-
-  public <T> void sendMessage(T message, ChatService chatService) {
-    sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
-  }
 }
