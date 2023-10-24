554009b981a1ce4a40d3a4994456e6e163a148d4
YeonSang <leeys12181811@gmail.com>
2023-10-24 16:51:40 +0900

    redis 적용

--- green/src/main/java/com/greengrim/green/core/chatRoom/ChatRoom.java
+++ green/src/main/java/com/greengrim/green/core/chatRoom/ChatRoom.java
@@ -1,12 +1,15 @@
 package com.greengrim.green.core.chatRoom;
 
+import java.io.Serializable;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+  private static final long serialVersionUID = 6494678977089006639L;
   private String roomId;
   private String name;
 
