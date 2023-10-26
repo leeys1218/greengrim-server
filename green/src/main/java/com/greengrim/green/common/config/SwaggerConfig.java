63dfae8a015ff66f8ca20e112526d310dcd094f7
YeonSang <leeys12181811@gmail.com>
2023-10-26 12:26:45 +0900

    스웨거 request url 변경 - 각 서버의 도메인 이름

--- green/src/main/java/com/greengrim/green/common/config/SwaggerConfig.java
+++ green/src/main/java/com/greengrim/green/common/config/SwaggerConfig.java
@@ -2,13 +2,17 @@ package com.greengrim.green.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-    info = @Info(title = "GreenGrim Server's API",
+        servers = {@Server(url = "https://www.greengrim.store", description = "Prod Server URL"),
+                   @Server(url = "https://dev.greengrim.store", description = "Develop Server URL"),
+                   @Server(url = "http://localhost:8080", description = "Local Server URL")} ,
+        info = @Info(title = "GreenGrim Server's API",
         description = "그린그림 서버 API 명세서",
         version = "v1"))
 @RequiredArgsConstructor
