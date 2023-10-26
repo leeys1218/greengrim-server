99a345fd367b97caf689a7ab0d58bce172409cfe
bora <youonl5@naver.com>
2023-10-26 13:12:57 +0900

    Feat: 내 프로필 조회 API

---
+++ green/src/main/java/com/greengrim/green/core/member/controller/GetMemberController.java
@@ -0,0 +1,28 @@
+package com.greengrim.green.core.member.controller;
+
+import com.greengrim.green.common.auth.CurrentMember;
+import com.greengrim.green.core.member.Member;
+import com.greengrim.green.core.member.dto.MemberResponseDto.MemberSimpleInfo;
+import com.greengrim.green.core.member.usecase.GetMemberUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GetMemberController {
+
+    private final GetMemberUseCase getMemberUseCase;
+
+    /**
+     * [GET] 내 프로필 조회
+     */
+    @GetMapping("/visitor/info")
+    public ResponseEntity<MemberSimpleInfo> getCurrentMemberInfo(
+            @CurrentMember Member member) {
+        return new ResponseEntity<>(getMemberUseCase.getMemberInfo(member),
+                HttpStatus.OK);
+    }
+}
--- green/src/main/java/com/greengrim/green/core/member/dto/MemberResponseDto.java
+++ green/src/main/java/com/greengrim/green/core/member/dto/MemberResponseDto.java
@@ -1,7 +1,9 @@
 package com.greengrim.green.core.member.dto;
 
+import com.greengrim.green.core.member.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class MemberResponseDto {
 
@@ -15,7 +17,23 @@ public class MemberResponseDto {
     @Getter
     @AllArgsConstructor
     public static class CheckNickNameRes {
-
         private boolean isUsed;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MemberSimpleInfo {
+        private Long id;
+        private String nickName;
+        private String introduction;
+        private String profileImgUrl;
+
+        public MemberSimpleInfo(Member member) {
+            this.id = member.getId();
+            this.nickName = member.getNickName();
+            this.introduction = member.getIntroduction();
+            this.profileImgUrl = member.getProfileImgUrl();
+        }
     }
 }
