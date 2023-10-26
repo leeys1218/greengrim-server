c3cb909029acb242ddc9e4d02181bd1f843774fb
bora <youonl5@naver.com>
2023-10-26 16:30:33 +0900

    Fix: BaseTime 미작동 버그 수정

--- green/src/main/java/com/greengrim/green/common/entity/BaseTime.java
+++ green/src/main/java/com/greengrim/green/common/entity/BaseTime.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -17,7 +18,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTime {
+public abstract class BaseTime implements Serializable {
 
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
