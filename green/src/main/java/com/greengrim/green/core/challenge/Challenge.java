141603ebc9e976ea8fcf3a0120f2430ea1339cd8
bora <youonl5@naver.com>
2023-10-26 17:04:39 +0900

    Feat: Challenge Entity

---
+++ green/src/main/java/com/greengrim/green/core/challenge/Challenge.java
@@ -0,0 +1,62 @@
+package com.greengrim.green.core.challenge;
+
+import com.greengrim.green.common.entity.BaseTime;
+import com.greengrim.green.core.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Challenge extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 50)
+    private String title;
+    @Size(max = 200)
+    private String description;
+    private String imgUrl;
+    @NotNull
+    private Category category;
+    @NotNull
+    private int goalCount;          // 목표 인증 횟수
+    @NotNull
+    private int ticketTotalCount;   // 총 티켓 개수
+    @NotNull
+    private int ticketCurrentCount; // 현재 남은 티켓 개수
+    @NotNull
+    @Min(2) @Max(6)
+    private int weekMinCount;       // 주 최소 인증 횟수
+    @NotNull
+    @Max(100)
+    private int capacity;           // 수용 가능 인원
+    @NotNull
+    @Max(100)
+    private int headCount;          // 현재 인원
+    @NotNull
+    private String keyword;
+    private boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+}
