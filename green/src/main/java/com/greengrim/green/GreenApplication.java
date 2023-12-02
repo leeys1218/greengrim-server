0846a956871781faeae8fbae25957167cc5ea306
YeonSang <leeys12181811@gmail.com>
2023-12-02 23:22:35 +0900

    add: 스프링 앱 시간을 KST로 변경

--- green/src/main/java/com/greengrim/green/GreenApplication.java
+++ green/src/main/java/com/greengrim/green/GreenApplication.java
@@ -1,5 +1,7 @@
 package com.greengrim.green;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class GreenApplication {
+
+	@PostConstruct
+	public void started() {
+		// timezone KST 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreenApplication.class, args);
