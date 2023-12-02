243e766c90a342691e259518f920291891361f31
bora <youonl5@naver.com>
2023-12-02 23:22:55 +0900

    Feat: NftManager 가 없다면 생성
    @PostConstruct 어노테이션을 사용하여 한번만 실행

--- green/src/main/java/com/greengrim/green/common/kas/NftManager/NftManagerRepository.java
+++ green/src/main/java/com/greengrim/green/common/kas/NftManager/NftManagerRepository.java
@@ -1,5 +1,6 @@
 package com.greengrim.green.common.kas.NftManager;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface NftManagerRepository extends JpaRepository<NftManager, Long> {
 
     NftManager findNftManagerById(Long manageId);
+
+    boolean existsNftManagerById(Long id);
 }
--- green/src/main/java/com/greengrim/green/common/kas/NftManager/NftManagerService.java
+++ green/src/main/java/com/greengrim/green/common/kas/NftManager/NftManagerService.java
@@ -1,13 +1,24 @@
 package com.greengrim.green.common.kas.NftManager;
 
+import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.N;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 @RequiredArgsConstructor
 public class NftManagerService {
 
     private final NftManagerRepository nftManagerRepository;
+
+    @PostConstruct
+    public void registerNftManager() {
+        if(!nftManagerRepository.existsNftManagerById(1L)) {
+            nftManagerRepository.save(new NftManager(0L));
+        }
+    }
+
 
     public Long getNftIdAndPlusOne() {
         NftManager nftManager = nftManagerRepository.findNftManagerById(1L);
