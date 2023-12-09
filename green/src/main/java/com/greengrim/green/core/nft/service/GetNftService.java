f0bcca4984aba2f4d793ee03a846ee05bd5e7981
bora <youonl5@naver.com>
2023-12-09 23:52:14 +0900

    Feat: NFT 조회 API 페이징에 SORT 추가

--- green/src/main/java/com/greengrim/green/core/nft/service/GetNftService.java
+++ green/src/main/java/com/greengrim/green/core/nft/service/GetNftService.java
@@ -1,7 +1,12 @@
 package com.greengrim.green.core.nft.service;
 
+import static com.greengrim.green.common.entity.SortOption.ASC;
+import static com.greengrim.green.common.entity.SortOption.DESC;
+
+import com.greengrim.green.common.entity.SortOption;
 import com.greengrim.green.common.entity.dto.PageResponseDto;
 import com.greengrim.green.common.exception.BaseException;
+import com.greengrim.green.common.exception.errorCode.GlobalErrorCode;
 import com.greengrim.green.common.exception.errorCode.NftErrorCode;
 import com.greengrim.green.core.member.Member;
 import com.greengrim.green.core.nft.Nft;
@@ -16,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,9 +43,8 @@ public class GetNftService {
         return new NftDetailInfo(nft, price, isMine);
     }
 
-    public HomeNfts getHomeNfts(Member member, int size) {
-        Pageable pageable = PageRequest.of(0, size);
-        Page<Nft> nfts = nftRepository.findHomeNfts(pageable);
+    public HomeNfts getHomeNfts(Member member, int page, int size, SortOption sortOption) {
+        Page<Nft> nfts = nftRepository.findHomeNfts(getPageable(page, size, sortOption));
 
         List<HomeNftInfo> homeNftInfoList = new ArrayList<>();
         nfts.forEach(nft ->
@@ -52,19 +57,17 @@ public class GetNftService {
      * 핫 NFTS 더보기
      * TODO: @param member 를 이용해 차단 목록에 있다면 보여주지 않기
      */
-    public PageResponseDto<List<HomeNftInfo>> getMoreHotNfts(Member member, int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Nft> nfts = nftRepository.findHomeNfts(pageable);
+    public PageResponseDto<List<HomeNftInfo>> getMoreHotNfts(Member member, int page, int size, SortOption sortOption) {
+        Page<Nft> nfts = nftRepository.findHomeNfts(getPageable(page, size, sortOption));
         return makeNftsInfoList(nfts);
     }
 
     /**
-     * 핫 NFTS 더보기
+     * 내 NFTS 더보기
      * TODO: @param member 를 이용해 차단 목록에 있다면 보여주지 않기
      */
-    public PageResponseDto<List<HomeNftInfo>> getMyHotNfts(Member member, int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Nft> nfts = nftRepository.findMyNfts(member, pageable);
+    public PageResponseDto<List<HomeNftInfo>> getMyHotNfts(Member member, int page, int size, SortOption sortOption) {
+        Page<Nft> nfts = nftRepository.findMyNfts(member, getPageable(page, size, sortOption));
         return makeNftsInfoList(nfts);
     }
 
@@ -82,6 +85,16 @@ public class GetNftService {
         } else {
             // TODO: Market Entity 추가 이후에, 가격을 double -> String으로 변환하는 로직 추가
             return "가격 여기에";
+        }
+    }
+
+    private Pageable getPageable(int page, int size, SortOption sort) {
+        if (sort == DESC) { // 최신순
+            return PageRequest.of(page, size, Sort.Direction.DESC, "createdAt");
+        } else if (sort == ASC) { // 오래된순
+            return PageRequest.of(page, size, Sort.Direction.ASC, "createdAt");
+        } else {
+            throw new BaseException(GlobalErrorCode.NOT_VALID_ARGUMENT_ERROR);
         }
     }
 
