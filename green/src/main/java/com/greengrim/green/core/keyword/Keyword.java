f7d96394f6e4eca5ad1ec93c44e7d8df08c2d363
YeonSang <leeys12181811@gmail.com>
2023-12-10 00:40:04 +0900

    fix: 키워드 엔티티 수정

--- green/src/main/java/com/greengrim/green/core/keyword/Keyword.java
+++ green/src/main/java/com/greengrim/green/core/keyword/Keyword.java
@@ -2,30 +2,35 @@ package com.greengrim.green.core.keyword;
 
 import com.greengrim.green.common.entity.BaseTime;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Keyword extends BaseTime {
+
+    public enum keywordType {
+        STYLE, VERB, NOUN;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    private Long memberId;
-    @NotNull
     private String keyword;
+    @NotNull
+    @Enumerated(value = EnumType.STRING)
+    private keywordType type;
 
-    public Keyword(Long memberId, String keyword) {
-        this.memberId = memberId;
+    public Keyword(String keyword, keywordType type) {
         this.keyword = keyword;
+        this.type = type;
     }
 }
